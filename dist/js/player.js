618790ab4d936e53bccf483c1202a222a61b45c7
Peter K Joseph <peterkjoseph02@gmail.com>
2023-01-17 22:41:55 +0530

    Deleted PHP dependency support

--- dist/js/player.js
+++ dist/js/player.js
@@ -14,7 +14,7 @@ let systemPlayer = {
 const syncTrack = () => {
     $("#song_list").append(`<div class="w-100 h-100 align-center"><div><center><svg xmlns="http://www.w3.org/2000/svg" class="icon-poster" width="16" height="16"fill="currentColor" class="bi bi-inbox-fill" viewBox="0 0 16 16"><path d="M4.98 4a.5.5 0 0 0-.39.188L1.54 8H6a.5.5 0 0 1 .5.5 1.5 1.5 0 1 0 3 0A.5.5 0 0 1 10 8h4.46l-3.05-3.812A.5.5 0 0 0 11.02 4H4.98zm-1.17-.437A1.5 1.5 0 0 1 4.98 3h6.04a1.5 1.5 0 0 1 1.17.563l3.7 4.625a.5.5 0 0 1 .106.374l-.39 3.124A1.5 1.5 0 0 1 14.117 13H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .106-.374l3.7-4.625z" /></svg><h3>No Tracks Found</h3></center></div></div>`);
     console.info("[BACKGROUND SERVICES] Starting Sync");
-    $.get(`${window.location}/../../server/song_list.json`)
+    $.post(`${window.location}/../../server/song_list.json`)
         .done((val) => {
         $("#currently_playing_name").html("Syncing Tracks");
         $("#currently_playing_artist").html("We are currently updating tracks. Please Wait");
